2671c63f9285b3ee4cc669f8426adb7965b4229b
Jérémy Lal <kapouer@melix.org>
2020-08-19 01:44:54 +0200

    front: much simpler controls

--- public/js/controls.js
+++ public/js/controls.js
@@ -1,41 +1,18 @@
 export default class Controls {
 	constructor(root) {
 		this.root = root;
-		this.filters = root.querySelectorAll('.filter');
-		this.addEventListener('change', this, false);
+		root.querySelector('.live-controls').addEventListener('change', this, false);
 	}
 
 	handleEvent(e) {
-		console.log(e.target.name);
+		if (e.target.name == "filter") {
+			this.root.classList.toggle("essentiel", e.target.value == "essentiel");
+		} else if (e.target.name == "sort") {
+			const messages = this.root.querySelectorAll('.live-message:not(.live-new):not(.pinned)');
+			// TODO procrastify each node before insertion
+			for (var node of Array.from(messages).reverse()) {
+				this.root.appendChild(node);
+			}
+		}
 	}
-
-
-	changeFilter(node) {
-		this.filters.forEach((filter) => filter.classList.toggle('checked', this == node));
-	}
-
 }
-
-
-	// filters.eq(0).click(function() {
-	// 	$parent.find('.live-message').removeClass('hide');
-	// 	$parent.procrastify('update');
-	// });
-	// filters.eq(1).click(function() {
-	// 	$parent.find('.live-message').not('.essentiel').addClass('hide');
-	// 	$parent.procrastify('update');
-	// });
-	// controls.find('.sort > *').click(function() {
-	// 	var messages = $parent.find('.live-message').not('.live-new,.pinned');
-	// 	messages.find('iframe.twitter-tweet-rendered').each(function() {
-	// 		if (!this.id) return;
-	// 		var oldTweet = $(this).contents();
-	// 		$(this).one('load', function() {
-	// 			$(this).contents().find('html').empty().append(oldTweet.children());
-	// 		});
-	// 	});
-	// 	var start = messages.first().prev();
-	// 	start.after($.makeArray(messages).reverse());
-	// 	$parent.procrastify('update');
-	// 	$(this).toggleClass('checked');
-	// });
